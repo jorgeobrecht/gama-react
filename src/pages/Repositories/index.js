742761fd32777d0850e43da5306a132c272cd5b7
Jorge Obrecht <jorgeobrecht@gmail.com>
2021-07-16 14:29:47 -0300

    Organização, boas práticas e componentes de estilo

---
+++ src/pages/Repositories/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import * as S from './styled';
+
+export default function Repositories() {
+   return(
+   <S.Container >
+    <S.Title>Repositorios</S.Title>
+    <S.List>
+        <S.ListItem>Repositorios: Nome Repositorio</S.ListItem>
+        <S.ListItem>Repositorios: Nome Repositorio</S.ListItem>
+        <S.ListItem>Repositorios: Nome Repositorio</S.ListItem>
+        <S.ListItem>Repositorios: Nome Repositorio</S.ListItem>
+        <S.ListItem>Repositorios: Nome Repositorio</S.ListItem>
+        <S.ListItem>Repositorios: Nome Repositorio</S.ListItem>
+        <S.ListItem>Repositorios: Nome Repositorio</S.ListItem>
+        <S.ListItem>Repositorios: Nome Repositorio</S.ListItem>
+        <S.ListItem>Repositorios: Nome Repositorio</S.ListItem>
+        <S.ListItem>Repositorios: Nome Repositorio</S.ListItem>
+    </S.List>
+    </S.Container>
+    )
+}
+
