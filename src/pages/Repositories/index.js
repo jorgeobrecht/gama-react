1ffdb3a45f799d6e53819014ede81e79c56537fd
Jorge Obrecht <jorgeobrecht@gmail.com>
2021-07-16 14:56:41 -0300

    Renderizando repositórios e programação declarativa

--- src/pages/Repositories/index.js
+++ src/pages/Repositories/index.js
@@ -1,21 +1,25 @@
-import React from "react";
+import React,{useEffect,useState}from "react";
 import * as S from './styled';
 
 export default function Repositories() {
+    const[repositories, setRepositories]=useState([]);
+
+    useEffect(()=>{
+        let repositoriesName=localStorage.getItem('repositoriesName');
+        repositoriesName=JSON.parse(repositoriesName);
+        setRepositories(repositoriesName);
+        localStorage.clear();
+    },[]);
+
    return(
    <S.Container >
     <S.Title>Repositorios</S.Title>
     <S.List>
-        <S.ListItem>Repositorios: Nome Repositorio</S.ListItem>
-        <S.ListItem>Repositorios: Nome Repositorio</S.ListItem>
-        <S.ListItem>Repositorios: Nome Repositorio</S.ListItem>
-        <S.ListItem>Repositorios: Nome Repositorio</S.ListItem>
-        <S.ListItem>Repositorios: Nome Repositorio</S.ListItem>
-        <S.ListItem>Repositorios: Nome Repositorio</S.ListItem>
-        <S.ListItem>Repositorios: Nome Repositorio</S.ListItem>
-        <S.ListItem>Repositorios: Nome Repositorio</S.ListItem>
-        <S.ListItem>Repositorios: Nome Repositorio</S.ListItem>
-        <S.ListItem>Repositorios: Nome Repositorio</S.ListItem>
+        {repositories.map(repository=>{
+            return(
+                <S.ListItem>Repositorio: {repository}</S.ListItem>
+            )
+        })}
     </S.List>
     </S.Container>
     )
